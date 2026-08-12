9f1ae289e2eef1f35c5ebd844e0a4dac4f70eaf5
Jorge Vergara Stuardo <jor.vergaras@duocuc.cl>
2026-08-11 23:23:44 -0400

    add new section for sanguchon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-
 import react from '@astrojs/react';
 
 // https://astro.build/config
@@ -9,7 +8,11 @@ export default defineConfig({
   site: 'https://www.sanguchonoficial.cl',
   vite: {
     plugins: [tailwindcss()],
+    build: {
+      target: 'esnext',
+      cssCodeSplit: true,
+      assetsInlineLimit: 4096,
+    },
   },
-
   integrations: [react()],
 });
