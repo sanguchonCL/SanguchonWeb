37105ddaee4436cfe5004fd78aef58690a439b03
Jorge Vergara Stuardo <jor.vergaras@duocuc.cl>
2026-09-08 19:39:44 -0300

    refactor: centralize latest TikTok video configuration

--- src/data/site-info.ts
+++ src/data/site-info.ts
@@ -24,6 +24,9 @@ export const SITE_INFO = {
     "https://www.google.com/maps/dir/?api=1&destination=San+Guchon+-+San+Nicol%C3%A1s+1390%2C+8920057+San+Miguel%2C+Regi%C3%B3n+Metropolitana",
   instagramUrl: "https://www.instagram.com/sanguchoncl/",
   tiktokUrl: "https://www.tiktok.com/@sanguchoncl",
+  latestTikTokVideoId: "7683291679553506578",
+  latestTikTokVideoUrl:
+    "https://www.tiktok.com/@sanguchoncl/video/7683291679553506578",
   menuUrl: "/menu-san-guchon.pdf",
   linkedinUrl:
     "https://www.linkedin.com/in/jorge-vergara-stuardo-1a894630b/?skipRedirect=true",
